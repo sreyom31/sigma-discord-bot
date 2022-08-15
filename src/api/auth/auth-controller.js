85de749c2a1afc97cd164606cecc5ed5242129fb
sreyom31 <sreyom31@gmail.com>
2022-08-16 02:29:43 +0530

    dev store
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/api/auth/auth-controller.js
+++ src/api/auth/auth-controller.js
@@ -1,9 +1,11 @@
+const store = require('store')
 const { getAccessToken, getUserDiscordProfile } = require('./auth-service');
 
 async function handleGetAccessToken(req, res) {
   try {
     const { code } = req.query;
     const data = await getAccessToken(code);
+    store.set('access_token',data.access_token); //! dev purpose only
     res.status(200).send(data);
   } catch (err) {
     console.log(err);
@@ -12,18 +14,19 @@ async function handleGetAccessToken(req, res) {
 }
 
 async function handleGetUserInfo(req, res) {
-  const header = req.headers['authorization'];
-  if (!header) {
-    return res.status(404).json({ message: 'No authorization header found' });
-  }
+  // const header = req.headers['authorization'];
+  // if (!header) {
+  //   return res.status(404).json({ message: 'No authorization header found' });
+  // }
 
-  const token = header.split(' ')[1];
-  if (!token) {
-    return res.status(404).json({ message: 'No authorization token found' });
-  }
+  // const token = header.split(' ')[1];
+  // if (!token) {
+  //   return res.status(404).json({ message: 'No authorization token found' });
+  // }
 
   try {
-    const user = await getUserDiscordProfile(token);
+    // const user = await getUserDiscordProfile(token);
+    const user = await getUserDiscordProfile(store.get('access_token')); //! dev purpose only
     if (!user) {
       return res.status(404).json({
         message: 'No user found',
